33bb8a7297213bd2b05ad0afd0a23db524e72336
Vadim Klimov <v.klimov@bastion-tech.ru>
2022-05-25 18:34:38 +0300

    Fix indents

--- include/connection.hpp
+++ include/connection.hpp
@@ -12,30 +12,30 @@ using tcp = boost::asio::ip::tcp;
 class connection : public std::enable_shared_from_this<connection>
 {
 public:
-	connection(tcp::socket&& socket, std::shared_ptr<abstract_router> router_ptr, const int64_t deadline);
+    connection(tcp::socket&& socket, std::shared_ptr<abstract_router> router_ptr, const int64_t deadline);
 
 public:
-	// start processing the connection
-	void start()
-	{
-		read_request();
-		check_deadline();
-	}
+    // start processing the connection
+    void start()
+    {
+        read_request();
+        check_deadline();
+    }
 
 private:
-	void read_request();
-	void write_response();
-	void check_deadline();
+    void read_request();
+    void write_response();
+    void check_deadline();
 
 private:
-	tcp::socket socket;
-	net::steady_timer timer;
+    tcp::socket socket;
+    net::steady_timer timer;
 
-	req_type request;
-	resp_type response;
-	beast::flat_buffer buffer;
+    req_type request;
+    resp_type response;
+    beast::flat_buffer buffer;
 
-	std::shared_ptr<abstract_router> router_ptr;
+    std::shared_ptr<abstract_router> router_ptr;
 
-	int64_t deadline;
+    int64_t deadline;
 };
